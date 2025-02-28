1d4a8c452c099143c4c9f76934f9155cfbde4479
Salvatore Misiano <104531745+SalvatoreMisia@users.noreply.github.com>
2025-02-28 02:14:03 +0100

    update 2

--- start-kernel.sh
+++ start-kernel.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-# detect the host
+# cleaning old connection files
+KERNEL_DIR="/usr/src/app/kernel"
+echo "Cleaning old connection files in $KERNEL_DIR"
+rm -f "$KERNEL_DIR"/*kernel-*.json
+
+:'# detect the host
 determine_target_ip() {
     # try to reach host.docker.internal
     if curl -s --connect-timeout 2 host.docker.internal > /dev/null; then
@@ -10,8 +15,8 @@ determine_target_ip() {
     fi
 }
 
-TARGET_IP=$(determine_target_ip)
-TARGET_IP="127.0.0.1" # to overwrite host.docker.internal, which it is no more working
+TARGET_IP=$(determine_target_ip)' # it is no more working, so we put directly 127.0.0.1
+TARGET_IP="127.0.0.1"
 
 # start the spyder kernel
 python -m spyder_kernels.console \
@@ -21,10 +26,10 @@ python -m spyder_kernels.console \
     --stdin=8890 \
     --control=8891 \
     --hb=8892 \
-    --IPKernelApp.connection_file=/usr/src/app/kernel/spyder-kernel-connection.json &
+    --IPKernelApp.connection_file="$KERNEL_DIR"/spyder-kernel-connection.json &
 
 # wait for json file to be created
-until KERNEL_FILE=$(ls /usr/src/app/kernel/*kernel-*.json 2> /dev/null); do
+until KERNEL_FILE=$(ls "$KERNEL_DIR"/*kernel-*.json 2> /dev/null); do
     sleep 1
 done
 
@@ -33,4 +38,5 @@ jq --arg ip "$TARGET_IP" '.ip = $ip' "$KERNEL_FILE" > "$KERNEL_FILE.tmp"
 mv "$KERNEL_FILE.tmp" "$KERNEL_FILE"
 
 # keep container active
+echo "Kernel is ready on 127.0.0.1"
 tail -f /dev/null
